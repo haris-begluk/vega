90594c050e310e6e375761d02eb198c36931346a
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-04-11 15:15:01 +0200

    Vega Initial Database Created

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -1,21 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using Vega.Models;
 
 namespace Vega.Persistence {
-    public class VegaDbContext :DbContext{ 
-
-    public VegaDbContext(DbContextOptions<VegaDbContext> options) :base(options)
+    public class VegaDbContext :DbContext{  
+        //Ako su klase povezane vezom kao sto su 
+        //Model i Make klase tada ce Entity prepoznati vezu i kreirati 
+        //odgovarajuce tabele
+        public DbSet<Make> Makes { get; set; }
+    public VegaDbContext(DbContextOptions<VegaDbContext> options)
+         :base(options)
         {   
             
         } 
-
-        /*       
-        public VegaDbContext(string connectionString):base(connectionString)
-        {   //Ovaj princip ne koristimo u dotnet core
-            //System.Configuration.ConfigurationManager
-        } 
-
-        */ 
-
-
     }
 }
